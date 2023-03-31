0ef7eca9ef0594cb26fa388c0f9738872649293e
EnderirT <83406577+triender@users.noreply.github.com>
2023-03-31 13:39:17 +0700

    Update BangCuuChuong.cpp

--- Week2/BangCuuChuong.cpp
+++ Week2/BangCuuChuong.cpp
@@ -8,100 +8,94 @@ private:
     int m, n;
 
 public:
-    BangCuuChuong(int m = 2, int n = 9)
+    BangCuuChuong(int m = -1, int n = -1)
     {
-        if (m < 2 || m > 9)
-            m = 2;
-        if (n < 2 || n > 9)
-            n = 9;
         this->m = m;
         this->n = n;
     }
 
-    void input()
+    void InBangCuuChuong()
     {
-        do
+        if (m == -1 && n == -1)
         {
-            cout << "Nhap m, n (2 <= m <= n <= 9): ";
-            cin >> m >> n;
-        } while (m < 2 || n > 9 || m > n);
-    }
+            // in ra hàng đầu tiên của bảng
+            cout << "    ";
+            for (int i = 1; i <= 9; i++)
+                cout << setw(3) << i << " ";
+            cout << endl;
 
-    void printBangCuuChuongM()
-    {
-        cout << "Bang cuu chuong " << m << ":" << endl;
-        for (int i = 1; i <= 10; ++i)
-        {
-            cout << m << " x " << i << " = " << m * i << endl;
-        }
-    }
+            // in ra dòng ngăn cách
+            cout << "   +-----------------------------------\n";
 
-    void printBangCuuChuongMN()
-    {
-        for (int i = m; i <= n; ++i)
-        {
-            cout << "Bang cuu chuong " << i << ":" << endl;
-            for (int j = 1; j <= 10; ++j)
+            // in ra các hàng còn lại của bảng
+            for (int i = 1; i <= 9; i++)
             {
-                cout << i << " x " << j << " = " << i * j << endl;
+                cout << setw(2) << i << " |";
+                for (int j = 1; j <= 9; j++)
+                    cout << setw(3) << i * j << " ";
+                cout << endl;
             }
-            cout << endl;
         }
-    }
-
-    void printBangCuuChuongTongHop()
-    {
-        // in ra hàng đầu tiên của bảng
-        cout << "   ";
-        for (int i = 1; i <= 9; i++)
-            cout << setw(3) << i << " ";
-        cout << endl;
-
-        // in ra dòng ngăn cách
-        cout << "   +-----------------------------------\n";
-
-        // in ra các hàng còn lại của bảng
-        for (int i = 1; i <= 9; i++)
+        else if (n == -1)
         {
-            cout << setw(2) << i << " |";
-            for (int j = 1; j <= 9; j++)
-                cout << setw(3) << i * j << " ";
-            cout << endl;
+            cout << "Bang cuu chuong " << m << ":" << endl;
+            for (int i = 1; i <= 10; ++i)
+                cout << m << " x " << i << " = " << m * i << endl;
+        }
+        else
+        {
+            for (int i = m; i <= n; ++i)
+            {
+                cout << "Bang cuu chuong " << i << ":" << endl;
+                for (int j = 1; j <= 10; ++j)
+                    cout << i << " x " << j << " = " << i * j << endl;
+                cout << endl;
+            }
         }
     }
 };
 
-int main() {
-    int choice;
-    BangCuuChuong bcc;
-
-    do {
+int main()
+{
+    int m = -1, n = -1, choice;
+    do
+    {
         cout << "MENU:\n";
-        cout << "1. In bang cuu chuong tu 2 den 9\n";
+        cout << "1. In bang cuu chuong tong hop\n";
         cout << "2. In bang cuu chuong tu m den n\n";
         cout << "3. In bang cuu chuong cua mot so m\n";
         cout << "4. Thoat chuong trinh\n";
         cout << "Nhap lua chon cua ban (1-4): ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                bcc.printBangCuuChuongTongHop();
-                break;
-            case 2:
-                bcc.input();
-                bcc.printBangCuuChuongMN();
-                break;
-            case 3:
-                bcc.input();
-                bcc.printBangCuuChuongM();
-                break;
-            case 4:
-                cout << "Cam on ban da su dung chuong trinh!\n";
-                break;
-            default:
-                cout << "Lua chon khong hop le. Vui long chon lai!\n";
-                break;
+        switch (choice)
+        {
+        case 1:
+        {
+            BangCuuChuong bcc(m, n);
+            bcc.InBangCuuChuong();
+            break;
+        }
+        case 2:
+        {
+            cin >> m;
+            BangCuuChuong bcc(m, n);
+            bcc.InBangCuuChuong();
+            break;
+        }
+        case 3:
+        {
+            cin >> m >> n;
+            BangCuuChuong bcc(m, n);
+            bcc.InBangCuuChuong();
+            break;
+        }
+        case 4:
+            cout << "Cam on ban da su dung chuong trinh!\n";
+            break;
+        default:
+            cout << "Lua chon khong hop le. Vui long chon lai!\n";
+            break;
         }
     } while (choice != 4);
 
